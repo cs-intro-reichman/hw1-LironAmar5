e3deddcb4f3cce7e607e410492c8a9b57555cc0c
Liron Amar <lironamar@Lirons-MacBook-Pro.local>
2025-10-31 15:34:35 +0200

    done

--- Bill3.java
+++ Bill3.java
@@ -4,15 +4,15 @@
 public class Bill3 {
     public static void main(String[] args) {
         
-        String name1 = args[0];
-        String name2 = args[1];
-        String name3 = args[2];
+        String a = args[0];
+        String b = args[1];
+        String c = args[2];
         int value = Integer.parseInt(args[3]);
 
         
         double share = Math.ceil((double)value / 3);
         
-		System.out.println("Dear " + name3 + ", " + name2 + " and " + name1 + ": pay " + share + " Shekels each");
+		System.out.println("Dear " + c + ", " + b + " and " + a + ": pay " + share + " Shekels each.");
         
 
         
--- NumWords.java
+++ NumWords.java
@@ -2,10 +2,10 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    int num = Integer.parseInt(args[0]);
-		
+		num = num % 1000;
 		int hundreds = num / 100;
 		int tens = (num % 100) / 10;
 		int ones = num % 10;
-		System.out.println(hundreds + " hundreds " + tens + " tens " + ones + " ones");
+		System.out.println(hundreds + ", hundreds " + tens + ", tens " + ones + " and ones");
 	}
 }
