16c894aa0c88bc2fec6c49620decb20f46ce0de1
Liron Amar <lironamar@Lirons-MacBook-Pro.local>
2025-10-31 02:35:09 +0200

    done

--- Ascend.java
+++ Ascend.java
@@ -1,7 +1,20 @@
+
+import com.sun.jdi.event.BreakpointEvent;
+import javax.lang.model.util.ElementScanner14;
+
 // Generates three integer random numbers in a given range,
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		int num1 = (int)(Math.random() * 10);
+		int num2 = (int)(Math.random() * 10);
+        int num3 = (int)(Math.random() * 10);
+
+		int min = Math.min(num3 , Math.min(num1 , num2));
+		int max = Math.max(num3 , Math.max(num1 , num2));
+		
+        int mid = (num1 + num2 + num3) - (max + min);
+
+	    System.out.println(min + " " + mid + " " + max);
 	}
 }
--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,14 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		
+		int currentValue = Integer.parseInt(args[0]);
+		double percentage = Double.parseDouble(args[1]) / 100;
+		int years = Integer.parseInt(args[2]);
+        
+		double futureValue = currentValue * Math.pow((1 + percentage), years);
+		System.out.println("After " + years + " years, $" + currentValue + " saved at " + percentage * 100 +"% will yield $" + (int)futureValue);
+
+
 	}
 }
